c5efc45ab7aff7b7f6e45398b03c7bf256132c8a
Xuxin2333 <zhuxuxinapp@gmail.com>
2022-12-05 16:11:11 +0100

    update login

---
+++ login.js
@@ -0,0 +1,34 @@
+//JavaScript pour le login
+
+
+class User{
+	constructor(idU,mdpU, roleU) {
+            this.idU = idU;
+            this.mdpU = mdpU;
+            this.roleU = roleU;
+        }
+        get idUser() {
+            return this.idU;
+        }
+        get mdpUser() {
+            return this.mdpU;
+        }
+        get roleUser() {
+            return this.roleU;
+        }
+}
+
+function 
+
+function login(){
+	var login=document.getElementById("idUser").value;
+	var mdp=document.getElementById("mdpUser").value;
+
+	if(login=="123" && mdp =="123"){
+		location.href="./main.html";
+	}
+	else{
+		alert("wrongggggggggggggggggggggggggggggggg");
+	}
+}
+
