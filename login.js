83974cd07c36f043bf4c755ede07b74cbb6171e5
Xuxin2333 <zhuxuxinapp@gmail.com>
2022-12-09 02:33:22 +0100

    update login(js ok no css)

--- login.js
+++ login.js
@@ -7,32 +7,54 @@ class User{
             this.mdpU = mdpU;
             this.roleU = roleU;
         }
-        get idUser() {
+        getIdUser(){
             return this.idU;
         }
-        get mdpUser() {
+        getMdpUser() {
             return this.mdpU;
         }
-        get roleUser() {
+        getRoleUser() {
             return this.roleU;
         }
 }
 
-function distruiberPage(name){
-    if(Object.keys(name).roleUser=="ADMIN"){
-        location.href="./main.html";
+
+const u1 = new User('test1', 'test1', 'ADMIN');
+const u2 = new User('test2', 'test2', 'ADMIN');
+const u3 = new User('test3', 'test3', 'USER');
+const u4 = new User('test4', 'test4', 'USER');
+const u5 = new User('test5', 'test5', 'USER');
+let users=new Array(u1,u2,u3,u4,u5);
+
+
+ 
+function distruiberPage(pfId,pfMdp){
+    const result = users.find(({ idU }) => idU === pfId);
+ 
+    if(typeof result =='undefined'){
+        alert("Something wrong with your id ");
     }
+    else{
+        if(result.getMdpUser()!=pfMdp){
+            alert("Your password is wrong");
+        }
+        else{
+        if(result.getRoleUser()=="ADMIN"){
+        location.href="./home_Admin.html";
+        }
+        if(result.getRoleUser()=="USER"){
+        location.href="./home_User.html";
+        }
+    }
+    }
+
 }
+
 
 function login(){
 	var login=document.getElementById("idUser").value;
 	var mdp=document.getElementById("mdpUser").value;
 
-	if(login=="123" && mdp =="123"){
-		location.href="./main.html";
-	}
-	else{
-		alert("wrongggggggggggggggggggggggggggggggg");
-	}
+	distruiberPage(login,mdp);
 }
 
